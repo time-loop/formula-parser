87f49ae79ee84a87d1123df29adb3d1733034681
Michal Michalowski <mmichalowski@clickup.com>
2024-05-16 22:46:14 +0200

    test: custom field functions tests

---
+++ test/unit/clickup/customField.test.ts
@@ -0,0 +1,26 @@
+import { getCustomFieldVariable, isCustomFieldVariable } from '../../../src/clickup/customField';
+
+describe('custom field functions', () => {
+    it.each([
+        ['CUSTOM_FIELD_1', true],
+        ['SOME_OTHER_VARIABLE', false],
+    ])('variable %s is a custom field variable: %s', (variableName, expected) => {
+        expect(isCustomFieldVariable(variableName)).toBe(expected);
+    });
+
+    it.each([
+        [
+            'value is correct',
+            'CUSTOM_FIELD_123',
+            { type: 'number', value: 10 },
+            { id: '123', type: 'number', value: 10 },
+        ],
+        ['variable is missing the type', 'CUSTOM_FIELD_123', { value: 10 }, undefined],
+        ['variable is missing the value', 'CUSTOM_FIELD_123', { type: 'number' }, undefined],
+        ['variable is not defined', 'CUSTOM_FIELD_123', undefined, undefined],
+        ['variable is null', 'CUSTOM_FIELD_123', null, undefined],
+    ])('correctly extracts field value when %s', (_, variable, value, expected) => {
+        const result = getCustomFieldVariable(variable, value);
+        expect(result).toEqual(expected);
+    });
+});
