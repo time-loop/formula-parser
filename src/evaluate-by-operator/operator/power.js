050260945c1c764b8e228a86d0f5768a091472ce
Michal Michalowski <mmichalowski@clickup.com>
2024-12-17 09:07:31 +0100

    feat: power: any unset value results in NaN

--- src/evaluate-by-operator/operator/power.js
+++ src/evaluate-by-operator/operator/power.js
@@ -1,9 +1,15 @@
 import { toNumber } from '../../helper/number';
 import { ERROR_VALUE } from '../../error';
+import { hasNil } from '../../clickup/utils';
 
 export const SYMBOL = '^';
 
 export default function func(exp1, exp2) {
+    if (hasNil(exp1, exp2)) {
+        // some of the arguments are unset, return NaN
+        return Number.NaN;
+    }
+
     const exp1Number = toNumber(exp1);
     const exp2Number = toNumber(exp2);
     if (exp1Number === undefined || exp2Number === undefined) {
